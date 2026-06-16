f3d2de59fed9861bc4a1aacebb32514da0cd1c6d
SaiPavan4321 <esp30370@gmail.com>
2026-06-16 14:38:36 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
-
+//cors are added
 const cors = require("cors");
 
 app.use(cors());
