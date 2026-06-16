1d293da32337730b10a4e276613a10d99d04205d
SaiPavan4321 <esp30370@gmail.com>
2026-06-16 14:16:21 +0530

    updated

--- frontend/script.js
+++ frontend/script.js
@@ -161,6 +161,27 @@ signupForm.addEventListener("submit", async (e) => {
 
 });
 
+function togglePassword(inputId, icon) {
+
+    const input = document.getElementById(inputId);
+
+    if (input.type === "password") {
+
+        input.type = "text";
+
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+
+    } else {
+
+        input.type = "password";
+
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+
+    }
+}
+
 signupTab.addEventListener("click",()=>{
 
 signupForm.classList.remove("hidden");
